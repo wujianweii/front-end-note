92098e1a6e33066a252851e4ab7e2bc2be074594
wujianweii <1121071856@qq.com>
2019-07-08 15:12:21 +0800

    fix

---
+++ Git.js
@@ -0,0 +1,17 @@
+Git 
+
+// Git常用命令总结 - https://blog.csdn.net/tomatozaitian/article/details/73515849
+
+// 创建文件
+// touch README.md / .gitignore
+
+
+// - https://www.cnblogs.com/syq816/p/9301700.html(删除)
+// 在本地仓库删除指定文件
+// rm 文件
+
+// 在本地仓库删除指定文件夹
+// rm -r 文件夹
+
+
+// 暂存区(git add - 增加到暂存区中) / 工作区(当前编辑) / 历史区(git hash)
